97e9777e301b0237355e37ea916474046ab2ba86
Jason <Jason@192.168.0.54>
2025-03-15 00:33:30 +0000

    Register Patient Panel placeholder textfields etc...

--- src/scheduler/Base1.java
+++ src/scheduler/Base1.java
@@ -30,6 +30,8 @@ public class Base1 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
         Home = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
@@ -45,9 +47,41 @@ public class Base1 extends javax.swing.JFrame {
         PatientListB = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         RegisterP = new javax.swing.JPanel();
+        jPanel8 = new javax.swing.JPanel();
+        jPanel5 = new javax.swing.JPanel();
+        jPanel7 = new javax.swing.JPanel();
+        jPanel4 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jList2 = new javax.swing.JList<>();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        Stylebar4 = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
+        returnRegisterP = new javax.swing.JLabel();
+        Stylebar5 = new javax.swing.JPanel();
+        jLabel8 = new javax.swing.JLabel();
         AdminP = new javax.swing.JPanel();
         SchedulingP = new javax.swing.JPanel();
         PatientListP = new javax.swing.JPanel();
+        Stylebar2 = new javax.swing.JPanel();
+        Stylebar3 = new javax.swing.JPanel();
+        jPanel6 = new javax.swing.JPanel();
+        jLabel7 = new javax.swing.JLabel();
+
+        jList1.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane2.setViewportView(jList1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -198,18 +232,207 @@ public class Base1 extends javax.swing.JFrame {
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/scheduler/HelpD.jpg"))); // NOI18N
         Home.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
-        RegisterP.setBackground(new java.awt.Color(153, 255, 255));
+        RegisterP.setBackground(new java.awt.Color(255, 255, 255));
+        RegisterP.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        javax.swing.GroupLayout RegisterPLayout = new javax.swing.GroupLayout(RegisterP);
-        RegisterP.setLayout(RegisterPLayout);
-        RegisterPLayout.setHorizontalGroup(
-            RegisterPLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jPanel8.setBackground(new java.awt.Color(0, 0, 0));
+
+        javax.swing.GroupLayout jPanel8Layout = new javax.swing.GroupLayout(jPanel8);
+        jPanel8.setLayout(jPanel8Layout);
+        jPanel8Layout.setHorizontalGroup(
+            jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 140, Short.MAX_VALUE)
+        );
+        jPanel8Layout.setVerticalGroup(
+            jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 20, Short.MAX_VALUE)
+        );
+
+        RegisterP.add(jPanel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 200, 140, 20));
+
+        jPanel5.setBackground(new java.awt.Color(0, 0, 0));
+
+        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
+        jPanel5.setLayout(jPanel5Layout);
+        jPanel5Layout.setHorizontalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 20, Short.MAX_VALUE)
+        );
+        jPanel5Layout.setVerticalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 20, Short.MAX_VALUE)
+        );
+
+        RegisterP.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(940, 200, 20, 20));
+
+        jPanel7.setBackground(new java.awt.Color(255, 255, 255));
+
+        jPanel4.setBackground(new java.awt.Color(204, 204, 204));
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 20, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel2.setText("Name:");
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel3.setText("Age:");
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel4.setText("Priority:");
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel5.setText("Room:");
+
+        jTextField1.setBackground(new java.awt.Color(204, 204, 204));
+        jTextField1.setText("jTextField1");
+
+        jTextField2.setBackground(new java.awt.Color(204, 204, 204));
+        jTextField2.setText("jTextField2");
+
+        jTextField3.setBackground(new java.awt.Color(204, 204, 204));
+        jTextField3.setText("jTextField3");
+
+        jTextField4.setBackground(new java.awt.Color(204, 204, 204));
+        jTextField4.setText("jTextField4");
+
+        jList2.setBackground(new java.awt.Color(204, 204, 204));
+        jList2.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane3.setViewportView(jList2);
+
+        jButton1.setText("jButton1");
+
+        jButton2.setText("jButton2");
+
+        javax.swing.GroupLayout jPanel7Layout = new javax.swing.GroupLayout(jPanel7);
+        jPanel7.setLayout(jPanel7Layout);
+        jPanel7Layout.setHorizontalGroup(
+            jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel7Layout.createSequentialGroup()
+                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(38, 38, 38)
+                .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel7Layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField3))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel7Layout.createSequentialGroup()
+                        .addComponent(jLabel5)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTextField4))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel7Layout.createSequentialGroup()
+                        .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 247, Short.MAX_VALUE)
+                            .addComponent(jTextField2)))
+                    .addGroup(jPanel7Layout.createSequentialGroup()
+                        .addComponent(jButton2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 151, Short.MAX_VALUE)
+                        .addComponent(jButton1)))
+                .addGap(53, 53, 53)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 363, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(25, Short.MAX_VALUE))
+        );
+        jPanel7Layout.setVerticalGroup(
+            jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel7Layout.createSequentialGroup()
+                .addGap(66, 66, 66)
+                .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel7Layout.createSequentialGroup()
+                        .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel5)
+                            .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton1)
+                            .addComponent(jButton2)))
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 337, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(37, Short.MAX_VALUE))
+        );
+
+        RegisterP.add(jPanel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 50, 800, 440));
+
+        Stylebar4.setBackground(new java.awt.Color(51, 51, 51));
+
+        jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("Register New Patients");
+
+        returnRegisterP.setIcon(new javax.swing.ImageIcon(getClass().getResource("/scheduler/left-arrow.png"))); // NOI18N
+        returnRegisterP.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                returnRegisterPMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout Stylebar4Layout = new javax.swing.GroupLayout(Stylebar4);
+        Stylebar4.setLayout(Stylebar4Layout);
+        Stylebar4Layout.setHorizontalGroup(
+            Stylebar4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Stylebar4Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(returnRegisterP)
+                .addGap(342, 342, 342)
+                .addComponent(jLabel6)
+                .addContainerGap(401, Short.MAX_VALUE))
+        );
+        Stylebar4Layout.setVerticalGroup(
+            Stylebar4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Stylebar4Layout.createSequentialGroup()
+                .addGroup(Stylebar4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(returnRegisterP)
+                    .addComponent(jLabel6))
+                .addGap(0, 5, Short.MAX_VALUE))
+        );
+
+        RegisterP.add(Stylebar4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 960, 30));
+
+        Stylebar5.setBackground(new java.awt.Color(51, 51, 51));
+
+        javax.swing.GroupLayout Stylebar5Layout = new javax.swing.GroupLayout(Stylebar5);
+        Stylebar5.setLayout(Stylebar5Layout);
+        Stylebar5Layout.setHorizontalGroup(
+            Stylebar5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 960, Short.MAX_VALUE)
         );
-        RegisterPLayout.setVerticalGroup(
-            RegisterPLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 530, Short.MAX_VALUE)
+        Stylebar5Layout.setVerticalGroup(
+            Stylebar5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 20, Short.MAX_VALUE)
         );
+
+        RegisterP.add(Stylebar5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 510, -1, -1));
+
+        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/scheduler/HelpD.jpg"))); // NOI18N
+        jLabel8.setText("jLabel8");
+        RegisterP.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 960, -1));
 
         AdminP.setBackground(new java.awt.Color(0, 204, 153));
 
@@ -236,17 +459,54 @@ public class Base1 extends javax.swing.JFrame {
         );
 
         PatientListP.setBackground(new java.awt.Color(255, 204, 204));
+        PatientListP.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        javax.swing.GroupLayout PatientListPLayout = new javax.swing.GroupLayout(PatientListP);
-        PatientListP.setLayout(PatientListPLayout);
-        PatientListPLayout.setHorizontalGroup(
-            PatientListPLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        Stylebar2.setBackground(new java.awt.Color(51, 51, 51));
+
+        javax.swing.GroupLayout Stylebar2Layout = new javax.swing.GroupLayout(Stylebar2);
+        Stylebar2.setLayout(Stylebar2Layout);
+        Stylebar2Layout.setHorizontalGroup(
+            Stylebar2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 960, Short.MAX_VALUE)
         );
-        PatientListPLayout.setVerticalGroup(
-            PatientListPLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 530, Short.MAX_VALUE)
+        Stylebar2Layout.setVerticalGroup(
+            Stylebar2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 20, Short.MAX_VALUE)
         );
+
+        PatientListP.add(Stylebar2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 510, -1, -1));
+
+        Stylebar3.setBackground(new java.awt.Color(51, 51, 51));
+
+        javax.swing.GroupLayout Stylebar3Layout = new javax.swing.GroupLayout(Stylebar3);
+        Stylebar3.setLayout(Stylebar3Layout);
+        Stylebar3Layout.setHorizontalGroup(
+            Stylebar3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 960, Short.MAX_VALUE)
+        );
+        Stylebar3Layout.setVerticalGroup(
+            Stylebar3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 30, Short.MAX_VALUE)
+        );
+
+        PatientListP.add(Stylebar3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 960, 30));
+
+        javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
+        jPanel6.setLayout(jPanel6Layout);
+        jPanel6Layout.setHorizontalGroup(
+            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 800, Short.MAX_VALUE)
+        );
+        jPanel6Layout.setVerticalGroup(
+            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 440, Short.MAX_VALUE)
+        );
+
+        PatientListP.add(jPanel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 50, 800, 440));
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/scheduler/HelpD.jpg"))); // NOI18N
+        jLabel7.setText("jLabel7");
+        PatientListP.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 960, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -337,6 +597,13 @@ public class Base1 extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_AdminBMouseClicked
 
+    private void returnRegisterPMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_returnRegisterPMouseClicked
+        
+ RegisterP.setVisible(false);
+     Home.setVisible(true);
+// TODO add your handling code here:
+    }//GEN-LAST:event_returnRegisterPMouseClicked
+
     /**
      * @param args the command line arguments
      */
@@ -385,11 +652,38 @@ public class Base1 extends javax.swing.JFrame {
     private javax.swing.JPanel SchedulingP;
     private javax.swing.JPanel Stylebar;
     private javax.swing.JPanel Stylebar1;
+    private javax.swing.JPanel Stylebar2;
+    private javax.swing.JPanel Stylebar3;
+    private javax.swing.JPanel Stylebar4;
+    private javax.swing.JPanel Stylebar5;
     private javax.swing.JPanel ViewP;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JList<String> jList1;
+    private javax.swing.JList<String> jList2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
+    private javax.swing.JPanel jPanel6;
+    private javax.swing.JPanel jPanel7;
+    private javax.swing.JPanel jPanel8;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JLabel returnRegisterP;
     // End of variables declaration//GEN-END:variables
 }
